4dc2143eb829a1aedaf1dfe6dbe1b09c3a541733
Praneeth <vprnth9@gmail.com>
2023-01-16 21:15:02 -0600

    client folder structure and initial routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,20 @@
+import HomePage from "containers/homePage";
+import LoginPage from "containers/loginPage";
+import ProfilePage from "containers/profilePage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
