61591440d65c758b21ce3f61ff45c41306e34162
Praneeth <vprnth9@gmail.com>
2023-01-16 19:41:23 -0600

    user routes

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -35,8 +35,8 @@ export const register = async (req, res) => {
 
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
 
@@ -44,15 +44,15 @@ export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
-    if (!user) return res.status(400).json({ msg: "User does not exist." });
+    if (!user) return res.status(400).json({ message: "User does not exist." });
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) res.status(400).json({ msg: "Invalid Credentials." });
+    if (!isMatch) res.status(400).json({ message: "Invalid Credentials." });
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
     delete user.password;
     res.status(200).json({ token, user });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
